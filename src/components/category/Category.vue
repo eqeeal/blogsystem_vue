75a1d1bc30f6f007e79e68699e180f802429a4f7
Next() <1727728279@qq.com>
2023-06-30 11:23:05 +0800

    登录完善

--- src/components/category/Category.vue
+++ src/components/category/Category.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <h1>标签管理</h1>
-    <div>
-
+    <div class="out-div">
+      <!-- 顶部页面标题 -->
+      <h1>分类管理</h1><br>
+      <!-- 页面主体内容 -->
+      <div class="main-content">
+        <div>左边添加分类</div>
+        <div>右边显示分类信息</div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,9 +22,24 @@ export default {
 </script>
 
 <style scoped>
-    
 h1 {
     display: flex;
     justify-content: flex-start;
+}
+.out-div {
+  background-color: white;
+  width: inherit;
+  height: inherit;
+  padding: 1px 10px 10px 10px;
+  border: 1px solid white;
+  border-radius: 5px;
+}
+.main-content {
+  width: inherit;
+  height: inherit;
+  /* background-color: ; */
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
 }
 </style>
